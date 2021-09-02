2699ae697d0510e8e013b954d3513ebb7986bd7e
Sathishkumar Jayaprakash <770902@cognizant.com>
2021-09-02 22:50:34 +0530

    Props Basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+//CSS
+import './index.css'
+//Setup Vars
+const firstBook = {
+  author: 'Rhonda Byrne',
+  title: 'The Secret',
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/61mEFHtyGVL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg',
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const secondBook = {
+  author: 'Sataz',
+  title: 'The Peace',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51yIFhF3RFL._SX487_BO1,204,203,200_.jpg',
+}
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      ></Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      ></Book>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+    </section>
+  )
+}
+
+const Book = ({ img, title, author }) => {
+  //const { img, title, author } = props;
+  //console.log(props)
+  return (
+    <article>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
